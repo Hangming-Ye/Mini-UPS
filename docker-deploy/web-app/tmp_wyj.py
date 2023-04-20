1ad22a46ad47e2c358a07d4fbcffa911771b6df9
Ewanka <yuanjia.wang@duke.edu>
2023-04-19 22:36:23 -0400

    change2func

--- docker-deploy/web-app/tmp_wyj.py
+++ docker-deploy/web-app/tmp_wyj.py
@@ -14,30 +14,38 @@ from google.protobuf.internal.encoder import _EncodeVarint
 
 seq = 0
 ack_set = set()
-worldid = 0
+ 
+'''
+@Desc   :Connect to the world
+@Arg    :None
+@Return :worldid
+'''
+def connect2world():
+    world_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    world_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    ip_port = ('0.0.0.0', 12345)
+    try:
+        world_socket.connect(ip_port)
+        print("Socket Connected")
+    except:
+        print("Connection error")
+        exit(0)
 
-world_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-world_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-ip_port = ('0.0.0.0', 12345)
-try:
-    world_socket.connect(ip_port)
-    print("Socket Connected")
-except:
-    print("error")
-    exit
+    uconnect = world_ups_pb2.UConnect()
+    uconnect.isAmazon = False
+    #send UConnect to world
+    send_msg(world_socket, uconnect.SerializeToString())
+    print("Sent UConnect")
 
-uconnect = world_ups_pb2.UConnect()
-uconnect.isAmazon = False
-#send UConnect to world
-send_msg(world_socket, uconnect.SerializeToString())
-print("Sent UConnect")
+    #receive UConnected from world
+    received = recv_msg(world_socket)
 
-#receive UConnected from world
-received = recv_msg(world_socket)
+    uconnected = world_ups_pb2.UConnected()
+    uconnected.ParseFromString(received)
+    worldid = uconnected.worldid
+    
+    print("Received MSG: " + uconnected.result)
+    print("world id is " + str(worldid))
 
-uconnected = world_ups_pb2.UConnected()
-uconnected.ParseFromString(received)
-worldid = uconnected.worldid
+    return worldid
 
-print("Received MSG: " + uconnected.result)
-print("world id is " + str(worldid))
