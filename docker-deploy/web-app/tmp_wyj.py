5ea4d879629b983f9f49d19ddac6f55677048c4b
Ewanka <yuanjia.wang@duke.edu>
2023-04-20 13:27:01 -0400

    seperate func and add pickup

--- docker-deploy/web-app/tmp_wyj.py
+++ docker-deploy/web-app/tmp_wyj.py
@@ -14,13 +14,13 @@ from google.protobuf.internal.encoder import _EncodeVarint
 
 seq = 0
 ack_set = set()
- 
+
 '''
-@Desc   :Connect to the world
+@Desc   :Connect to server and get world_socket
 @Arg    :None
-@Return :worldid
+@Return :world_socket
 '''
-def connect2world():
+def connectServer():
     world_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     world_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     ip_port = ('0.0.0.0', 12345)
@@ -30,9 +30,19 @@ def connect2world():
     except:
         print("Connection error")
         exit(0)
+    return world_socket
 
+'''
+@Desc   :Connect to the world
+@Arg    :world_socket, Truck Number
+@Return :worldid
+'''
+def connectWorld(world_socket, truck_num):
     uconnect = world_ups_pb2.UConnect()
     uconnect.isAmazon = False
+    
+    #connect to db and initiate truck
+
     #send UConnect to world
     send_msg(world_socket, uconnect.SerializeToString())
     print("Sent UConnect")
@@ -43,9 +53,35 @@ def connect2world():
     uconnected = world_ups_pb2.UConnected()
     uconnected.ParseFromString(received)
     worldid = uconnected.worldid
-    
+
     print("Received MSG: " + uconnected.result)
     print("world id is " + str(worldid))
 
     return worldid
+
+'''
+@Desc   :Send UGoPickup command to the world
+@Arg    :world_socket, truckid, warehouseid, seqnum
+@Return :None
+'''
+def send_UGoPickup(world_socket, truckid, whid, seq):
+    ucommands = world_ups_pb2.UCommands()
+    ucommands.disconnect = False
+    pickup = ucommands.pickups.add()
+    pickup.truckid = truckid
+    pickup.whid = whid
+    pickup.seqnum = seq
+
+    #send UCommand to the world
+    while True:
+        send_msg(world_socket, ucommands.SerializeToString())
+        print("Sent UCommand go pick up")
+        #debug
+        time.sleep(4)
+        print(ack_set)
+        if seq in ack_set:
+            print("Sent UCommand go pick up, already received by world")
+        else:
+            print(" Sent UCommand go pick up, not received by world " + str(seq))
+
 
