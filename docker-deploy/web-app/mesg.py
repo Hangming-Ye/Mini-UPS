bac7e8dbef74471e64ef278935c467ea7f29e634
Ewanka <yuanjia.wang@duke.edu>
2023-04-19 20:51:49 -0400

    Connect to world

---
+++ docker-deploy/web-app/mesg.py
@@ -0,0 +1,28 @@
+import psycopg2
+import socket
+import world_ups_pb2, U2A_pb2
+import sys
+import threading
+import select
+
+from google.protobuf.internal.decoder import _DecodeVarint32
+from google.protobuf.internal.encoder import _EncodeVarint
+
+def send_msg(socketfd, msg):
+    _EncodeVarint(socketfd.send, len(msg), None)
+    socketfd.sendall(msg)
+
+def recv_msg(socket):
+    var_int_buff = []
+    while True:
+        try:
+            buf = socket.recv(1)
+            var_int_buff += buf
+            msg_len, new_pos = _DecodeVarint32(var_int_buff, 0)
+            if new_pos != 0:
+                break
+        except:
+            whole_message = []
+            return whole_message
+    whole_message = socket.recv(msg_len)
+    return whole_message
---
+++ docker-deploy/web-app/tmp_wyj.py
@@ -0,0 +1,42 @@
+import psycopg2
+import socket
+import world_ups_pb2, U2A_pb2
+import sys
+import threading
+import select
+from mesg import *
+import smtplib
+from email.mime.text import MIMEText
+import time
+
+from google.protobuf.internal.decoder import _DecodeVarint32
+from google.protobuf.internal.encoder import _EncodeVarint
+
+seq = 0
+ack_set = set()
+worldid = 0
+
+world_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+world_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+ip_port = ('vcm-31032.vm.duke.edu', 12345)
+try:
+    world_socket.connect(ip_port)
+    print("Socket Connected")
+except socket.error:
+    print("Caught exception socket.error")
+
+uconnect = world_ups_pb2.UConnect()
+uconnect.isAmazon = False
+#send UConnect to world
+send_msg(world_socket, uconnect.SerializeToString())
+print("Sent UConnect")
+
+#receive UConnected from world
+received = recv_msg(world_socket)
+
+uconnected = world_ups_pb2.UConnected()
+uconnected.ParseFromString(received)
+worldid = uconnected.worldid
+
+print("Received MSG: " + uconnected.result)
+print("world id is " + str(worldid))
